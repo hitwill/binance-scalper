67bb5a8e55b3c485162e74eb609d89511cc242c6
Will Gikandi <will@lend.com.au>
2020-05-31 01:37:30 +1000

    Compile

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,9 @@ const client = binance_api_node_1.default({
     apiSecret: process.env.API_SECRET,
 });
 let priceTicker = []; //hold a list of recent prices
-let standardDeviation = 0;
+let minTickerLength = 10; //min number of prices to use for calculation
+let quantile = { upper: Infinity, lower: 0 };
+let findEntry = false;
 let currentFee = { maker: Infinity, taker: Infinity };
 let assets = {
     quoteAsset: {
@@ -18,7 +20,7 @@ let assets = {
         minPrice: Infinity,
         maxPrice: 0,
         tickSize: 0,
-        takeProfitPips: 0
+        takeProfitPips: 0,
     },
     baseAsset: {
         name: process.env.BASE_ASSET,
@@ -31,9 +33,8 @@ let assets = {
 let tradingSymbol = assets.baseAsset.name + assets.quoteAsset.name;
 async function start() {
     await getExchangeInfo(); //populate variables
-    assets.quoteAsset.takeProfitPips = getTakeProfitPips();
-    console.log(currentFee, assets);
-    //listenMarket(); //start listening
+    assets.quoteAsset.takeProfitPips = calcTakeProfitPips();
+    listenMarket(); //start listening
 }
 async function getExchangeInfo() {
     let exchangeInfo = await client.exchangeInfo();
@@ -81,17 +82,32 @@ function extractRules(symbol) {
     }
 }
 function addPriceToTicker(price) {
-    priceTicker.unshift(price);
+    priceTicker.unshift(Number(price));
 }
 function calcStandardDev() {
+    let standardDeviation = 0;
     let newTicker = []; // we'll resize the ticker to fit the standard dev we can trade in
     for (let i = 0, size = priceTicker.length; i < size; i++) {
         newTicker.push(priceTicker[i]);
-        standardDeviation = mathjs_1.std(priceTicker);
-        if (standardDeviation >= assets.quoteAsset.takeProfitPips)
+        if (i < minTickerLength) {
+            standardDeviation = 0;
+            continue;
+        }
+        standardDeviation = mathjs_1.std(newTicker);
+        //multiply deviate by 2 because it's one end, middle, then other end
+        if (standardDeviation * 2 >= assets.quoteAsset.takeProfitPips) {
+            findEntry = true;
             break; //ticker is long enought
+        }
+        else {
+            findEntry = false;
+        }
     }
     priceTicker = newTicker; // resize the ticker
+}
+function calcQuantile() {
+    quantile.lower = toPrecision(mathjs_1.quantileSeq(priceTicker, 0.01), assets.quoteAsset.precision, false);
+    quantile.upper = toPrecision(mathjs_1.quantileSeq(priceTicker, 0.99), assets.quoteAsset.precision, false);
 }
 function toPrecision(num, digits, roundUpwards) {
     let precise;
@@ -101,33 +117,47 @@ function toPrecision(num, digits, roundUpwards) {
         let position = str.indexOf('.') + digits + 1;
         tail = Number(str.slice(position, position + 1));
         if (tail > 0) {
-            precise = Number(str.slice(0, position)) + Number('0.'.padEnd(digits + 1, '0') + '1');
+            precise =
+                Number(str.slice(0, position)) +
+                    Number('0.'.padEnd(digits + 1, '0') + '1');
         }
         else {
             precise = Number(str.slice(0, position));
         }
     }
     else {
-        precise = Number(num.toFixed(digits)); //round as normail
+        precise = Number(Number(num).toFixed(digits)); //round as normail
     }
     return precise;
 }
-function getMinProfitPips() {
+function calcMinProfitPips() {
     //formula below comes from: profit = volume(sellingPrice = buyingPrice).((100-fee)/100)
     let profit = assets.quoteAsset.tickSize; //just one pip
     let pips = (100 * profit) / (100 - currentFee.taker); //convert to pips
     pips = toPrecision(pips, assets.quoteAsset.precision, true); //set precission and round it up
     return pips;
 }
-function getTakeProfitPips() {
-    let minProfit = getMinProfitPips(); //our takeProfit is just the mininimum profit we can get (scalping)
+function calcTakeProfitPips() {
+    let minProfit = calcMinProfitPips(); //our takeProfit is just the mininimum profit we can get (scalping)
     return minProfit; //we can add rules to increase profit later
+}
+function setPosition() {
+    if (findEntry == false)
+        return;
+    if (priceTicker[0] <= quantile.lower) {
+        console.log('buy at: ' + priceTicker[0]);
+    }
+    if (priceTicker[0] >= quantile.upper) {
+        console.log('sell at: ' + priceTicker[0]);
+    }
 }
 function listenMarket() {
     client.ws.aggTrades([tradingSymbol], (trade) => {
+        console.log(trade.price);
         addPriceToTicker(trade.price);
         calcStandardDev();
-        console.log(standardDeviation, priceTicker);
+        calcQuantile();
+        setPosition();
     });
 }
 start();
